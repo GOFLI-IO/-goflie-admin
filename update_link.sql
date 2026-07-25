21f8b6a7a4892c550b7fc004070235fc52733743
GOFLI-IO <sintiahaya@gmail.com>
2026-07-25 21:43:34 +0700

    Delete update_link.sql

--- update_link.sql
+++
@@ -1,6 +0,0 @@
--- Upsert: create the link if it doesn't exist, or update its URL if it does
-INSERT INTO links (slug, original_url, title, created_at)
-VALUES ('1306576f-32d6-45c7-8f09-48eb4d07cfa2', 'https://goflie-me.store', '', strftime('%s','now') * 1000)
-ON CONFLICT(slug) DO UPDATE SET
-  original_url = excluded.original_url,
-  title = excluded.title;
